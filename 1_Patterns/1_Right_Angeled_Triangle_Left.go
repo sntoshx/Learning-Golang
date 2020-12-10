1163d2dc628ab21c99fc27e2ca013d85f2422ab5
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-11 02:00:50 +0530

    Pattern #1 Uploaded

---
+++ 1_Patterns/1_Right_Angeled_Triangle_Left.go
@@ -0,0 +1,15 @@
+package main
+import (
+	"fmt"
+)
+var height int
+func main() {
+	fmt.Print("Enter the height of the triangle: ")
+	fmt.Scanln(&height)
+	for i:=1; i<=height; i++ {
+		for j:=0; j<i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
