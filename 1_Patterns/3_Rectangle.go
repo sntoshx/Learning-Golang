7443f8c21a8a218e13bde57c65e185a7330f3a9d
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-11 03:55:28 +0530

    Pattern #3 Uploaded

---
+++ 1_Patterns/3_Rectangle.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+var l, b int
+func main() {
+	fmt.Print("Enter length and breadth: ")
+	fmt.Scan(&l,&b)
+	for i:=0; i<b; i++ {
+		for j:=0; j<l; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println("")
+	}
+}
