81d9deaf6ddd5cec92763885d9753c44b9f0de18
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-11 11:46:17 +0530

    Pattern #4 Uploaded

---
+++ 1_Patterns/4_Isosceles_Triangle_Based_Down.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt")
+var height int
+func main() {
+	fmt.Print("Enter the height of triangle: ")
+	fmt.Scan(&height)
+	for i:=1; i<=height;i++ {
+		for j:=0; j<=height-i; j++ {
+			fmt.Print(" ")
+		}
+		for j:=0; j< 2*(i)-1; j++{
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
