6f53bca54247b9d4737bc0e73123cd53bb6dbe21
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-12 18:28:03 +0530

    Pattern #8 Uploading

---
+++ 1_Patterns/8_Diamond.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+var height int
+
+func main() {
+	fmt.Print("Enter the height of the triangle: ")
+	fmt.Scan(&height)
+	for i := 0; i < height-1; i++ {
+		for j := 0; j < height - i-1; j++ {
+			fmt.Print(" ")
+		}
+		for j:=0; j<i+1; j++{
+			fmt.Print(" *")
+		}
+		fmt.Println("")
+	}
+	for i := 0; i < height; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print(" ")
+		}
+		for j:=0; j<height-i; j++{
+			fmt.Print(" *")
+		}
+		fmt.Println("")
+	}
+}
+
