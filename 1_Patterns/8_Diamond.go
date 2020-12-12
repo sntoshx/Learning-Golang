1b2dc859977c473d0ee73452ce4e74af23546615
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-12 18:39:11 +0530

    Uploading

--- 1_Patterns/8_Diamond.go
+++ 1_Patterns/8_Diamond.go
@@ -7,7 +7,7 @@ import (
 var height int
 
 func main() {
-	fmt.Print("Enter the height of the triangle: ")
+	fmt.Print("Enter the width of the diamond: ")
 	fmt.Scan(&height)
 	for i := 0; i < height-1; i++ {
 		for j := 0; j < height - i-1; j++ {
