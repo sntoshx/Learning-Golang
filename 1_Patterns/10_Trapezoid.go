cca6bb1e07b7dbc4178148a1a4bf4e9d76c4609f
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-13 20:23:52 +0530

    Uploading

--- 1_Patterns/10_Trapezoid.go
+++ 1_Patterns/10_Trapezoid.go
@@ -4,5 +4,14 @@ var height int
 func main() {
 
 	fmt.Print("Enter a height for the trapezoid: ")
-	
+	fmt.Scan(&height)
+	for i:=0; i<height; i++ {
+		 for j:=0; j<i; j++ {
+			 fmt.Print(" ")
+		 }
+		 for j:=0; j<height-i+(2*height +2); j++{
+			 fmt.Print("  *")
+		 }
+		 fmt.Println("")
+	}
 }
