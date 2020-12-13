e66dfe7f00ec56d471717bbae874b78e02312a8f
Santosh Pandey <yednaphsotnas@gmail.com>
2020-12-13 20:31:21 +0530

    Uploading

--- 1_Patterns/8_Scalene_Obtuse_Triangle.go
+++ 1_Patterns/8_Scalene_Obtuse_Triangle.go
@@ -4,9 +4,9 @@ var height int
 func main() {
 	fmt.Print("Enter a height: ")
 	fmt.Scan(&height)
-	for i:=0; i<height; i++ {
+	for i:=1; i<height+1; i++ {
 		for j:=0; j<i; j++{
-			fmt.Print("  ")
+			fmt.Print(" ")
 		}
 		for j:=0; j<i; j++{
 			fmt.Print(" *")
